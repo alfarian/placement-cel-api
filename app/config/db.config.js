8c37c5610f61dc94f0646e7dca4b72c11f4388d5
vibeoski <adarsh97vtk@gmail.com>
2020-11-22 16:16:11 +0530

    db config changed

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  HOST: "sql12.freemysqlhosting.net",
-  USER: "sql12376663",
-  PASSWORD: "7BVEGDmGGS",
-  DB: "sql12376663"
+  HOST: "remotemysql.com",
+  USER: "wAUxVVBgKu",
+  PASSWORD: "uYBkRZTEgS",
+  DB: "wAUxVVBgKu"
 };
